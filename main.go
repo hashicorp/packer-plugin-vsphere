78855c844b2e812a3f99ff8047f094df418bb333
sylviamoss <moss@hashicorp.com>
2021-06-14 17:42:05 +0200

    put on 1.0.1 dev mode

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "0.0.2"
+	Version = "1.0.1"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
