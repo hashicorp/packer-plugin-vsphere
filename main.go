41fcbb91bdc2bd3958d584842473d9ea6e040fb9
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 16:04:19 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -5,25 +5,12 @@ import (
 	"os"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
+
 	"github.com/hashicorp/packer-plugin-vsphere/builder/vsphere/clone"
 	"github.com/hashicorp/packer-plugin-vsphere/builder/vsphere/iso"
 	"github.com/hashicorp/packer-plugin-vsphere/post-processor/vsphere"
 	vsphere_template "github.com/hashicorp/packer-plugin-vsphere/post-processor/vsphere-template"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.2"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-vsphere/version"
 )
 
 func main() {
@@ -32,7 +19,7 @@ func main() {
 	pps.RegisterBuilder("clone", new(clone.Builder))
 	pps.RegisterPostProcessor(plugin.DEFAULT_NAME, new(vsphere.PostProcessor))
 	pps.RegisterPostProcessor("template", new(vsphere_template.PostProcessor))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
