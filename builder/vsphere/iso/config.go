b8876dd0914da5a0cf5fc8b68e941b531ed23ac7
Pablo Caraballo Llorente <pcaraballo@vmware.com>
2022-07-07 14:29:22 +0200

    Allow deleting VM after vsphere-iso completion

--- builder/vsphere/iso/config.go
+++ builder/vsphere/iso/config.go
@@ -52,6 +52,8 @@ type Config struct {
 	// The import doesn't work if [convert_to_template](#convert_to_template) is set to true.
 	ContentLibraryDestinationConfig *common.ContentLibraryDestinationConfig `mapstructure:"content_library_destination"`
 
+	CleanupConfig `mapstructure:",squash"`
+
 	ctx interpolate.Context
 }
 
@@ -101,6 +103,8 @@ func (c *Config) Prepare(raws ...interface{}) ([]string, error) {
 	if c.ContentLibraryDestinationConfig != nil {
 		errs = packersdk.MultiErrorAppend(errs, c.ContentLibraryDestinationConfig.Prepare(&c.LocationConfig)...)
 	}
+
+	errs = packersdk.MultiErrorAppend(errs, c.CleanupConfig.Prepare()...)
 
 	if len(errs.Errors) > 0 {
 		return warnings, errs
