1cd12f9c15e09b9ea9e756d9dc6e02d2521affae
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2024-03-21 16:17:18 +0300

    common: use cdroms len instead of ISOPaths len
    There's special case when option `remove_cdrom` is set that there
    would be zero cdrom devices by the time `reattach_cdroms` processing
    is executed, so the comparison to ISOPaths len is wrong. Instead use
    the amount of cdroms we have at that point.
    
    Fixes: https://github.com/hashicorp/packer-plugin-vsphere/issues/393

--- builder/vsphere/common/step_reattach_cdrom_test.go
+++ builder/vsphere/common/step_reattach_cdrom_test.go
@@ -41,18 +41,16 @@ func TestStepReattachCDRom_Run(t *testing.T) {
 			expectedAction: multistep.ActionContinue,
 			vmMock: &driver.VirtualMachineMock{
 				ReattachCDRomsCalled: true,
+				CdromDevicesList:     object.VirtualDeviceList{nil},
 			},
 			expectedVmMock: &driver.VirtualMachineMock{
 				EjectCdromsCalled:        true,
-				CdromDevicesList:         object.VirtualDeviceList{nil, nil, nil, nil, nil, nil},
+				CdromDevicesCalled:       true,
+				CdromDevicesList:         object.VirtualDeviceList{nil, nil, nil, nil},
 				ReattachCDRomsCalled:     true,
 				FindSATAControllerCalled: true,
-				AddCdromCalledTimes:      6,
-				AddCdromTypes: []string{
-					"sata", "sata",
-					"sata", "sata",
-					"sata", "sata",
-				},
+				AddCdromCalledTimes:      3,
+				AddCdromTypes:            []string{"sata", "sata", "sata"},
 			},
 			fail: false,
 		},
@@ -80,6 +78,7 @@ func TestStepReattachCDRom_Run(t *testing.T) {
 			expectedVmMock: &driver.VirtualMachineMock{
 				RemoveNCdromsCalled:  true,
 				EjectCdromsCalled:    true,
+				CdromDevicesCalled:   true,
 				ReattachCDRomsCalled: true,
 				AddCdromCalledTimes:  0,
 				CdromDevicesList:     object.VirtualDeviceList{nil, nil},
@@ -103,11 +102,14 @@ func TestStepReattachCDRom_Run(t *testing.T) {
 			expectedAction: multistep.ActionContinue,
 			vmMock: &driver.VirtualMachineMock{
 				ReattachCDRomsCalled: true,
+				CdromDevicesList:     object.VirtualDeviceList{nil, nil},
 			},
 			expectedVmMock: &driver.VirtualMachineMock{
 				EjectCdromsCalled:    true,
+				CdromDevicesCalled:   true,
 				ReattachCDRomsCalled: true,
 				AddCdromCalledTimes:  0,
+				CdromDevicesList:     object.VirtualDeviceList{nil, nil},
 			},
 			fail: false,
 		},
