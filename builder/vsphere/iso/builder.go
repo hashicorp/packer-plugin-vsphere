18ccc75978fbf5f60300fc5f4aec99f43ac4d36d
Ryan Johnson <ryan@tenthirtyam.org>
2025-02-06 16:42:24 -0500

    chore: common iso godoc
    Adds or updates the Go Doc comments for the common/iso functions.
    
    Signed-off-by: Ryan Johnson <ryan@tenthirtyam.org>

--- builder/vsphere/iso/builder.go
+++ builder/vsphere/iso/builder.go
@@ -20,8 +20,10 @@ type Builder struct {
 	runner multistep.Runner
 }
 
+// ConfigSpec returns an HCL2 object specification based on the Builder's configuration mapping.
 func (b *Builder) ConfigSpec() hcldec.ObjectSpec { return b.config.FlatMapstructure().HCL2Spec() }
 
+// Prepare processes the given raw inputs, validates the configuration, and returns warnings or errors if any occur.
 func (b *Builder) Prepare(raws ...interface{}) ([]string, []string, error) {
 	warnings, errs := b.config.Prepare(raws...)
 	if errs != nil {
@@ -31,6 +33,9 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, []string, error) {
 	return nil, warnings, nil
 }
 
+// Run executes the build process steps for the `Builder`, leveraging the provided context, UI, and lifecycle hook.
+// It initializes state, configures steps sequentially, and manages interactions with the virtual machine driver.
+// Returns a finalized artifact or an error if the build process fails.
 func (b *Builder) Run(ctx context.Context, ui packersdk.Ui, hook packersdk.Hook) (packersdk.Artifact, error) {
 	state := new(multistep.BasicStateBag)
 	state.Put("debug", b.config.PackerDebug)
