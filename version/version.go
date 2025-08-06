61939fd656247a95accbc1c405dcac4ab4913b62
karthik P <karthik200499@gmail.com>
2025-08-06 14:55:43 +0530

    version: cut release v2.0.0

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
 	Version           = "2.0.0"
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
