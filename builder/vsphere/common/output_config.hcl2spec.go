ea36fdf042cf88fff7e80395dfaf81b55676f88c
Ryan Johnson <ryan@tenthirtyam.org>
2025-10-15 23:21:48 -0400

    feat: add support for datastore clusters
    Adds support for the use of datastore clusters in the applicable builders and post-processors.
    
    Signed-off-by: Ryan Johnson <ryan@tenthirtyam.org>

--- builder/vsphere/common/output_config.hcl2spec.go
+++ builder/vsphere/common/output_config.hcl2spec.go
@@ -3,7 +3,7 @@
 package common
 
 import (
-	"io/fs"
+	"os"
 
 	"github.com/hashicorp/hcl/v2/hcldec"
 	"github.com/zclconf/go-cty/cty"
@@ -12,8 +12,8 @@ import (
 // FlatOutputConfig is an auto-generated flat version of OutputConfig.
 // Where the contents of a field with a `mapstructure:,squash` tag are bubbled up.
 type FlatOutputConfig struct {
-	OutputDir *string      `mapstructure:"output_directory" required:"false" cty:"output_directory" hcl:"output_directory"`
-	DirPerm   *fs.FileMode `mapstructure:"directory_permission" required:"false" cty:"directory_permission" hcl:"directory_permission"`
+	OutputDir *string     `mapstructure:"output_directory" required:"false" cty:"output_directory" hcl:"output_directory"`
+	DirPerm   os.FileMode `mapstructure:"directory_permission" required:"false" cty:"directory_permission" hcl:"directory_permission"`
 }
 
 // FlatMapstructure returns a new FlatOutputConfig.
@@ -29,7 +29,7 @@ func (*OutputConfig) FlatMapstructure() interface{ HCL2Spec() map[string]hcldec.
 func (*FlatOutputConfig) HCL2Spec() map[string]hcldec.Spec {
 	s := map[string]hcldec.Spec{
 		"output_directory":     &hcldec.AttrSpec{Name: "output_directory", Type: cty.String, Required: false},
-		"directory_permission": &hcldec.AttrSpec{Name: "directory_permission", Type: cty.Number, Required: false},
+		"directory_permission": &hcldec.AttrSpec{Name: "directory_permission", Type: cty.Bool, Required: false}, /* TODO(azr): could not find type */
 	}
 	return s
 }
