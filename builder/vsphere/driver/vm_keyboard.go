45ed0f24f3af569c108b0d5888bea0e545fffbe0
Ryan Johnson <ryan.johnson@broadcom.com>
2026-01-27 16:45:33 -0500

    chore(xfer): update headers (#592)
    Post-transfer update of the copyright and SPDX headers.
    
    Signed-off-by: Ryan Johnson <ryan.johnson@broadcom.com>

--- builder/vsphere/driver/vm_keyboard.go
+++ builder/vsphere/driver/vm_keyboard.go
@@ -1,4 +1,5 @@
-// Copyright IBM Corp. 2013, 2025
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
 // SPDX-License-Identifier: MPL-2.0
 
 package driver
